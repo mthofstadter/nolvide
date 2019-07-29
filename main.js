7a39d89de28b32f075b1053618a73a5805c7a9ea
mthofstadter <31392780+mthofstadter@users.noreply.github.com>
2019-07-29 15:09:45 -0500

    GO AWAY TATE
    boxes fly off thanks to JS function. remindersBox grows

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 //woo
+
+function boxClick(element) {
+  var container = document.getElementById("boxMainContainer");
+  container.classList.add("leave");
+
+  var linkClass = element.className;
+  var box = document.getElementById(linkClass);
+  box.style.cssText = "animation-play-state: paused;";
+
+  //delay animation until boxes are off page
+  setTimeout(function(){ loadPage(linkClass);}, 1000);
+}
+
+function loadPage(name) {
+  console.log(name);
+  var box = document.getElementById(name);
+  box.classList.add("grow");
+}
