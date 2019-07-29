bfdce319526cf21c2edf52f2b92965d1502be474
mthofstadter <31392780+mthofstadter@users.noreply.github.com>
2019-07-29 15:27:29 -0500

    reminders animation... honestly wow
    smooth transition with fading text, but nolvide logo still is choppy on page load

--- main.js
+++ main.js
@@ -9,11 +9,12 @@ function boxClick(element) {
   box.style.cssText = "animation-play-state: paused;";
 
   //delay animation until boxes are off page
-  setTimeout(function(){ loadPage(linkClass);}, 1000);
+  setTimeout(function(){ loadPage(linkClass);}, 600);
 }
 
 function loadPage(name) {
   console.log(name);
   var box = document.getElementById(name);
   box.classList.add("grow");
+  setTimeout("location.href = 'reminders.html';",1400);
 }
