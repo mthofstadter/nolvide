aac65a27d544619d8f6e3aab6d66096594f99ae3
mthofstadter <31392780+mthofstadter@users.noreply.github.com>
2019-07-30 11:11:36 -0500

    keeping keep's groupHeader
    uses localStorage to save groupHeader for keep in touch

---
+++ keep.js
@@ -0,0 +1,20 @@
+//keep.js
+//mthofstadter.github.io/nolvide
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  init();
+});
+
+function init() {
+  if(localStorage.getItem('groupHeader') != null) {
+      document.getElementById('groupHeader').value = localStorage.getItem('groupHeader');
+  }
+}
+
+function save(input) {
+  var inputID = input.id;
+  var value = input.value;
+  console.log(inputID)
+  localStorage.setItem(inputID, value);
+}
