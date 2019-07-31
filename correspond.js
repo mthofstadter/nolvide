73aa6f776700949f2abddaa7bc9a05fc28363ca4
mthofstadter <31392780+mthofstadter@users.noreply.github.com>
2019-07-31 12:22:10 -0500

    checkboxes are fun
    just playing around

--- correspond.js
+++ correspond.js
@@ -17,9 +17,31 @@ function save(input) {
   var value = input.value;
   console.log(inputID)
   localStorage.setItem(inputID, value);
+  if(input.id == "days1") {
+    calculate()
+  }
 }
 
 function goHome() {
   document.getElementById('whiteBox').classList.add('grow');
   setTimeout("location.href='index.html';",1250);
+}
+
+
+function calculate(nameID) {
+  //startDate = nameSD
+  //days = nameDays
+  var startDate = nameID.concat("SD");
+  startDate = localStorage.getItem(startDate);
+  console.log(startDate);
+
+}
+
+function resetDays(checkbox) {
+  if(document.getElementById("name1").value == "Tofukima" || document.getElementById("name1").value == "tofukima" )  {
+    document.getElementById("body").classList.add("checked");
+    setTimeout("document.getElementById('body').classList.remove('checked');", 800);
+  }
+  setTimeout("checkbox.checked = false;", 800);
+
 }
