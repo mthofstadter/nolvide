ccf71a7e1b333c7f7f728d80eb76ce397c0a545e
mthofstadter <31392780+mthofstadter@users.noreply.github.com>
2019-07-31 10:26:24 -0500

    i want u to stay
    cards don't fly off on click anymore. Simplified clickBox() function as a result.

--- main.js
+++ main.js
@@ -2,25 +2,15 @@
 //mthofstadter.github.io/nolivde
 
 function hello() {
+  //this will be a welcome screen for first time
 }
+
 
 function boxClick(element) {
-  var container = document.getElementById("main");
-  container.classList.add("leave");
-
   var linkClass = element.className;
   var box = document.getElementById(linkClass);
-  box.style.cssText = "animation-play-state: paused;";
-
-  //delay animation
-  setTimeout(function(){ loadPage(linkClass);}, 300);
-}
-
-function loadPage(name) {
-  console.log(name);
-  var box = document.getElementById(name);
   box.classList.add("grow");
-  var url = name.slice(0,-3); //remove "Box" from name
+  var url = linkClass.slice(0,-3); //remove "Box" from name
   url = url.concat(".html"); //add ".html" to name
   setTimeout(`location.replace("${url}");`,1400);
 }
