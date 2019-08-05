f1681663e1ed874fb1a4a69828af269c1a57613a
mthofstadter <31392780+mthofstadter@users.noreply.github.com>
2019-08-05 10:55:35 -0500

    saaaave me -e
    finally saves the people. Arrays within arrays in localstorage don't work... Added delete functionality as well

--- correspond.js
+++ correspond.js
@@ -1,42 +1,62 @@
 //correspond.js
 //mthofstadter.github.io/nolvide
 
-var peopleArray = [];
-
 document.addEventListener("DOMContentLoaded", function(event) {
   init();
 });
 
 function init() {
-  peopleArray.push(document.getElementById("person0"));
-  console.log(peopleArray);
   //RESET
-  localStorage.setItem("peopleCount", 0);
+  //localStorage.setItem("peopleCount", 0);
   for(var i = 0; i < localStorage.length; i++) {
     //console.log(localStorage.getItem(localStorage.key(i)));
     if(document.getElementById(localStorage.key(i)) != null) { //If saved element exists on the page
       document.getElementById(localStorage.key(i)).value = localStorage.getItem(localStorage.key(i));
     }
   }
-  //peopleArray = JSON.parse(localStorage.getItem("peopleArray"));
+  restorePeople();
 }
 
-function save2(input) {
-  var inputID = input.id;
-  var value = input.value;
-  localStorage.setItem(inputID, value);
+function goHome() {
+  document.getElementById('whiteBox').classList.add('grow');
+  setTimeout("location.href='index.html';",1200);
+}
+
+function restorePeople() {
+  for(var i=1; i<=parseInt(localStorage.getItem("peopleCount")); i++) {
+    var checkThis = "person";
+    checkThis = checkThis.concat(i);
+    if(localStorage.getItem(checkThis) != null) {
+      restore(i);
+    }
+  }
 }
 
 function save(input) {
   var parent = input.parentElement;
+  var personArray =
+    {name:parent.children[0].value, days:parent.children[2].value, startDate:"SD"}
+  localStorage.setItem(parent.id, JSON.stringify(personArray));
+  var peopleArray = [];
+}
+
+
+function save3(input) {
+  var parent = input.parentElement;
   var num = input.id[input.id.length - 1]; //get number of person
+  var personArray = {}
   peopleArray.push(parent);
+  console.log(peopleArray);
+  console.log("json", JSON.stringify(peopleArray));
   localStorage.setItem("peopleArray", JSON.stringify(peopleArray));
+  var temp = localStorage.getItem("peopleArray");
+  console.log(temp);
+  peopleArray = JSON.parse(temp);
   console.log(peopleArray);
 }
 
 function add() {
-  var duplicate = document.getElementById("person0"); //always will have at least one
+  var duplicate = document.getElementById("exPerson");
   var newPerson = duplicate.cloneNode(true);
   var newCount =  parseInt(localStorage.getItem("peopleCount")) + 1;
   localStorage.setItem("peopleCount", newCount);
@@ -46,29 +66,70 @@ function add() {
   newPerson.id = textID.concat(newCount);
 
   textID = "name"
-  newPerson.children[0].value = "";
   newPerson.children[0].id = textID.concat(newCount);
 
   textID = "timer";
-  newPerson.children[1].value = "";
   newPerson.children[1].id = textID.concat(newCount);
 
   textID = "days";
-  newPerson.children[2].value = "";
   newPerson.children[2].id = textID.concat(newCount);
 
   textID = "checkbox";
-  newPerson.children[4].value = "";
   newPerson.children[4].id = textID.concat(newCount);
 
   document.getElementById("people").appendChild(newPerson);
-  localStorage.setItem("peopleArray", JSON.stringify(peopleArray));
 }
 
-function goHome() {
-  document.getElementById('whiteBox').classList.add('grow');
-  setTimeout("location.href='index.html';",1200);
+function restore(num) {
+  var duplicate = document.getElementById("exPerson");
+  var newPerson = duplicate.cloneNode(true);
+
+  var textID = "person";
+  newPerson.id = textID.concat(num);
+  access = JSON.parse(localStorage.getItem(newPerson.id));
+
+  textID = "name"
+  newPerson.children[0].id = textID.concat(num);
+  newPerson.children[0].value = access["name"];
+
+  textID = "timer";
+  newPerson.children[1].id = textID.concat(num);
+
+  textID = "days";
+  newPerson.children[2].id = textID.concat(num);
+  newPerson.children[2].value = access["days"];
+
+  textID = "checkbox";
+  newPerson.children[4].id = textID.concat(num);
+
+  document.getElementById("people").appendChild(newPerson);
 }
+
+var editing = false;
+
+function edit() {
+  if(editing == false) {
+    document.getElementById("people").classList.add("editMode");
+    editing = true;
+  }
+  else {
+    document.getElementById("people").classList.remove("editMode");
+    editing = false;
+  }
+}
+
+function checkMe(element) {
+  if(editing == true) {
+    var parent = element.parentElement;
+    localStorage.removeItem(parent.id);
+    console.log(parent.id);
+    parent.style.display = "none";
+  }
+  else {
+    console.log("not editing");
+  }
+}
+
 
 
 function calculate(nameID) {
