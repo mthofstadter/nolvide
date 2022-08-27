cdfd06a92aa9c324887be351296b208154f26c5a
mthofstadter <31392780+mthofstadter@users.noreply.github.com>
2022-08-27 15:49:47 -0500

    Multiple Groups!!
    - Redid localStorage
    - mutliple groups possible
    - minor styling changes

--- correspond.js
+++ correspond.js
@@ -10,11 +10,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
 function init() {
   //RESET
   // localStorage.clear();
-  if(localStorage.getItem("peopleCount") == null) {
-    localStorage.setItem("peopleCount", 0);
+  if(localStorage.getItem("groupsArray") == null) { //on first visit
+    let groupsArray = [[]];
+    let groupsNames = [""];
+    localStorage.setItem("groupsArray", JSON.stringify(groupsArray));
+    localStorage.setItem("groupsNames", JSON.stringify(groupsNames));
   }
-  restorePeople();
-  basicRestore();
+  restoreLoop();
 }
 
 function goHome() {
@@ -22,113 +24,149 @@ function goHome() {
   setTimeout("location.href='index.html';",1200);
 }
 
-function restorePeople() {
-  for(var i=0; i<=parseInt(localStorage.getItem("peopleCount")); i++) {
-    var checkThis = "person";
-    checkThis = checkThis.concat(i);
-    if(localStorage.getItem(checkThis) != null) {
-      restore(i);
+function restoreLoop() {
+  let groupsArray = accessGroups();
+  let groupsNames = accessNames();
+  for(let groupNum=0; groupNum<groupsArray.length; groupNum++) {
+    restoreEmptyGroup(groupNum);
+    document.getElementById("groupHeader-"+groupNum).value = groupsNames[groupNum];
+    for(let personNum=0; personNum<groupsArray[groupNum].length; personNum++) {
+      restore(groupNum, personNum);
     }
   }
+
+}
+
+function accessGroups() {
+  return JSON.parse(localStorage.getItem("groupsArray"));
+}
+
+function accessNames() {
+  return JSON.parse(localStorage.getItem("groupsNames"));
+}
+
+function restore(groupNum, personNum) {
+  let duplicate = document.getElementById("exPerson"); //exPerson = example to clone
+  let newPerson = duplicate.cloneNode(true);
+
+  newPerson.id = "person-" + groupNum + "-" + personNum;
+
+  let groupsArray = accessGroups();
+  let personArray = groupsArray[groupNum][personNum];
+
+  newPerson.children[0].id = "name-" + groupNum + "-" + personNum;
+  newPerson.children[0].value = personArray["name"];
+
+  newPerson.children[1].id = "timer-" + groupNum + "-" + personNum;
+
+  newPerson.children[2].id = "days-" + groupNum + "-" + personNum;
+  newPerson.children[2].value = personArray["days"];
+
+  newPerson.children[4].id = "checkbox-" + groupNum + "-" + personNum;
+
+  document.getElementById("people-" + groupNum).appendChild(newPerson);
+  calculate(groupNum, personNum);
+}
+
+function restoreEmptyGroup(groupNum) {
+  let duplicate = document.getElementById("group-");
+  let newGroup = duplicate.cloneNode(true);
+  let newGroupChildren = newGroup.getElementsByTagName("*");
+
+  newGroup.id += groupNum;
+  for(let i=0; i<newGroupChildren.length; i++) {
+    newGroupChildren[i].id += groupNum;
+  }
+  document.getElementById("container").appendChild(newGroup);
 }
 
 function save(input) {
-  var parentID = input.parentElement.id;
-  var personArray = accessArray(parentID);
-  console.log(personArray);
-  var inputID = input.id;
-  inputID = inputID.replace(/[0-9]/g, ''); //remove numbers
+  let groupNum = input.id.split('-')[1];
+  let personNum = input.id.split('-')[2];
+  let groupsArray = accessGroups();
+  let personArray = groupsArray[groupNum][personNum];
+
+  let inputID = input.id.split('-')[0];
   personArray[inputID] = input.value;
-  localStorage.setItem(parentID, JSON.stringify(personArray));
-  calculate(parentID);
+  groupsArray[groupNum][personNum] = personArray;
+  localStorage.setItem("groupsArray", JSON.stringify(groupsArray));
+  calculate(groupNum, personNum);
 }
 
-function basicSave(input) {
-  localStorage.setItem(input.id, input.value);
+function saveGroupName(input) {
+  let groupNum = input.id.split('-').pop();
+  let groupsNames = accessNames();
+  groupsNames[groupNum] = input.value;
+  localStorage.setItem("groupsNames", JSON.stringify(groupsNames));
+  
 }
 
-function basicRestore() {
-  for(var i = 0; i < localStorage.length; i++) {
-    if(document.getElementById(localStorage.key(i)) != null) { //If saved element exists on the page
-      document.getElementById(localStorage.key(i)).value = localStorage.getItem(localStorage.key(i));
-    }
+
+function addPerson(group) {
+  group = group.parentElement;
+  let duplicate = document.getElementById("exPerson"); //exPerson = example to clone
+  let newPerson = duplicate.cloneNode(true);
+  let groupsArray =  JSON.parse(localStorage.getItem("groupsArray"));
+  let groupNum = group.id.split('-').pop();
+  let personNum = groupsArray[groupNum].length;
+
+  let textID = "person-";
+  newPerson.id = textID.concat(personNum);
+
+  let emptyArray = {name:"", days:"", startDate:new Date()}
+  groupsArray[groupNum].push(emptyArray);
+  localStorage.setItem("groupsArray", JSON.stringify(groupsArray));
+
+  newPerson.children[0].id = "name-" + groupNum + "-" + personNum;
+  newPerson.children[1].id = "timer-" + groupNum + "-" + personNum;
+  newPerson.children[2].id = "days-" + groupNum + "-" + personNum;
+  newPerson.children[4].id = "checkbox-" + groupNum + "-" + personNum;
+
+  document.getElementById("people-"+groupNum).appendChild(newPerson);
+}
+
+function addGroup() {
+  let groupsArray = accessGroups();
+  let groupsNames = accessNames();
+  let groupNum = groupsArray.length;
+  groupsArray.push([]);
+  groupsNames.push("");
+  localStorage.setItem("groupsArray",JSON.stringify(groupsArray));
+  localStorage.setItem("groupsNames",JSON.stringify(groupsNames));
+
+  let duplicate = document.getElementById("group-");
+  let newGroup = duplicate.cloneNode(true);
+  let newGroupChildren = newGroup.getElementsByTagName("*");
+
+  newGroup.id += groupNum;
+  for(let i=0; i<newGroupChildren.length; i++) {
+    console.log("hello" + i);
+    newGroupChildren[i].id += groupNum;
   }
+  document.getElementById("container").appendChild(newGroup);
+
 }
 
-function add() {
-  var duplicate = document.getElementById("exPerson"); //exPerson = example to clone
-  var newPerson = duplicate.cloneNode(true);
-  var newCount =  parseInt(localStorage.getItem("peopleCount")) + 1;
-  localStorage.setItem("peopleCount", newCount);
+function edit(editButton) {
+  let groupNum = editButton.id.split('-').pop();
+  let groupsArray = accessGroups();
 
-  var textID = "person";
-  newPerson.id = textID.concat(newCount);
-
-  var emptyArray = {name:"", days:"", startDate:new Date()}
-  emptyArray = JSON.stringify(emptyArray);
-  localStorage.setItem(newPerson.id, emptyArray);
-
-  textID = "name"
-  newPerson.children[0].id = textID.concat(newCount);
-
-  textID = "timer";
-  newPerson.children[1].id = textID.concat(newCount);
-
-  textID = "days";
-  newPerson.children[2].id = textID.concat(newCount);
-
-  textID = "checkbox";
-  newPerson.children[4].id = textID.concat(newCount);
-
-  document.getElementById("people").appendChild(newPerson);
-}
-
-function restore(num) {
-  var duplicate = document.getElementById("exPerson");
-  var newPerson = duplicate.cloneNode(true);
-
-  var textID = "person";
-  newPerson.id = textID.concat(num);
-  var access = JSON.parse(localStorage.getItem(newPerson.id));
-
-  textID = "name"
-  newPerson.children[0].id = textID.concat(num);
-  newPerson.children[0].value = access["name"];
-
-  textID = "timer";
-  newPerson.children[1].id = textID.concat(num);
-
-  textID = "days";
-  newPerson.children[2].id = textID.concat(num);
-  newPerson.children[2].value = access["days"];
-
-  textID = "checkbox";
-  newPerson.children[4].id = textID.concat(num);
-
-  document.getElementById("people").appendChild(newPerson);
-  calculate(newPerson.id);
-}
-
-function edit() {
   if(editing == false) {
-    document.getElementById("people").classList.add("editMode");
+    document.getElementById("people-"+groupNum).classList.add("editMode");
     editing = true;
-    for(let i=0; i<=parseInt(localStorage.getItem("peopleCount")); i++) {
-      let timer = document.getElementById("timer".concat(i));
+    for(let i=0; i<=groupsArray[groupNum].length; i++) {
+      let timer = document.getElementById("timer-"+groupNum+"-"+i);
       if(timer != null) {
         timer.innerHTML = "";
         editDate(timer);
-        // let ymd = accessArray(timer.parentElement.id).startDate;
-        // let ymdText = ymd.substring(5,7) + "/" + ymd.substring(8,10) + "/" + ymd.substring(0,4);
-        // timer.innerHTML = ymdText;
       }
     }
   }
   else {
-    document.getElementById("people").classList.remove("editMode");
+    document.getElementById("people-"+groupNum).classList.remove("editMode");
     editing = false;
-    for(let i=0; i<=parseInt(localStorage.getItem("peopleCount")); i++) {
-      let timer = document.getElementById("timer".concat(i));
+    for(let i=0; i<=groupsArray[groupNum].length; i++) {
+      let timer = document.getElementById("timer-".concat(i));
       if(timer != null) {
         timer.removeChild(timer.firstChild);
       }
@@ -137,74 +175,74 @@ function edit() {
 }
 
 function checkMe(element) {
-  if(editing == true) {
-    var parent = element.parentElement;
-    localStorage.removeItem(parent.id);
-    console.log(parent.id);
-    parent.style.display = "none";
+  if(editing == true) { //in edit mode = delete
+    removePerson(element);
+    return;
   }
-  else {
-    var parent = element.parentElement;
-    var array1 = accessArray(parent.id);
-    array1["startDate"] = new Date();
-    // array1["startDate"] = new Date("Aug 15, 2022 15:00:00"); //reset for testing
-    localStorage.setItem(parent.id, JSON.stringify(array1));
-    calculate(parent.id);
-    parent.children[1].classList.add("drain");
-    element.classList.add("checkAnimate");
-    setTimeout(`${element.id}.checked = false;`, 500);
-    setTimeout(`${element.id}.classList.remove("checkAnimate");`, 1000);
+  let groupNum = element.parentElement.parentElement.id.split('-').pop();
+  let personNum = element.id.split('-').pop();
+  let groupsArray = accessGroups();
 
-  }
+  groupsArray[groupNum][personNum]["startDate"] = new Date();
+  localStorage.setItem("groupsArray", JSON.stringify(groupsArray));
+  calculate(groupNum, personNum);
+  element.parentElement.children[1].classList.add("drain");
+  element.classList.add("checkAnimate");
+  setTimeout(`document.getElementById("${element.id}").checked = false;`, 500);
+  setTimeout(`document.getElementById("${element.id}").classList.remove("checkAnimate");`, 1000);
+
 }
 
-function accessArray(personID) {
-  return JSON.parse(localStorage.getItem(personID));
+function removePerson(element) {  
+  let groupNum = element.id.split('-')[1];
+  let personNum = element.id.split('-').pop();
+  let groupsArray = accessGroups();
+  groupsArray[groupNum].splice(personNum,1); //removes person from array
+  localStorage.setItem("groupsArray", JSON.stringify(groupsArray));
+  element.parentElement.remove();
 }
 
-function calculate(nameID) {
-  var personArray = accessArray(nameID);
-  let elapsed = getElapsed(nameID);
+function calculate(groupNum, personNum) {
+  let groupsArray = accessGroups();
+  let personArray = groupsArray[groupNum][personNum];
+  let elapsed = getElapsed(groupNum, personNum);
 
-  var percent = Math.round(elapsed / parseInt(personArray["days"]) * 100);
-  var element = document.getElementById(nameID);
-  var inverse = 100 - percent;
+  let percent = Math.round(elapsed / parseInt(personArray["days"]) * 100);
+  let timer = document.getElementById("timer-"+groupNum + "-" + personNum);
+  let inverse = 100 - percent;
   if(percent > inverse) {
-    element.children[1].style.cssText =
+    timer.style.cssText =
       `background: linear-gradient(to left, #eeeeee ${percent}%, #17a772 ${inverse}%)`;
   }
   else {
-    element.children[1].style.cssText =
+    timer.style.cssText =
       `background: linear-gradient(to right, #17a772 ${inverse}%, #eeeeee ${percent}%)`;
   }
 
   if(percent >= 100) {
-    element.children[1].classList.add("empty");
+    timer.classList.add("empty");
   }
   else {
-    element.children[1].classList.remove("empty");
+    timer.classList.remove("empty");
   }
 }
 
-function test() {
-  document.getElementById("timer1").classList.add("fillTank");
-}
-
-function getElapsed(nameID) {
+function getElapsed(groupNum, personNum) {
   var currentDate = new Date();
-  //var currentDate = new Date("Aug 22, 2022 15:00:00")
-  var personArray = accessArray(nameID);
+  let groupsArray = accessGroups();
+  let personArray = groupsArray[groupNum][personNum];
   var startDate = Date.parse(personArray["startDate"]);
-  //var startDate = new Date("Aug 12, 2022 15:00:00");
   var secs = Math.abs(currentDate - startDate) / 1000;
   return Math.round(secs / 86400);
 }
 
 function showDate(timer) {
+  let groupNum = timer.parentElement.parentElement.id.split('-').pop();
+  let personNum = timer.id.split('-').pop();
   if(editing) {
     return;
   }
-  let elapsed = getElapsed(timer.parentElement.id);
+  let elapsed = getElapsed(groupNum, personNum);
   if(elapsed == 0) {
     timer.innerHTML = "Today";
   } else if(elapsed == 1) {
@@ -213,24 +251,37 @@ function showDate(timer) {
     timer.innerHTML = elapsed + " days ago";
   }
   timer.classList.add("showDate");
-  setTimeout(`${timer.id}.classList.remove("showDate");`, 2000);
+  setTimeout(`document.getElementById("${timer.id}").classList.remove("showDate");`, 2000);
 }
 
 function editDate(timer) {
+  let groupsArray = accessGroups();
+  let groupNum = timer.parentElement.parentElement.id.split('-').pop();
+  let personNum = timer.id.split('-').pop();
+
   const datePicker = document.createElement("input");
-  datePicker.id = "date" + timer.id.slice(-1);
+  datePicker.id = "date" + timer.id.split('-').pop();
   datePicker.classList.add("date");
   datePicker.type = "date";
   datePicker.setAttribute("onchange", "saveNewDate(this)");
-  datePicker.value = accessArray(timer.parentElement.id).startDate.split('T')[0];
+  datePicker.value = groupsArray[groupNum][personNum]["startDate"].split('T')[0];
   timer.appendChild(datePicker);
 }
 
 function saveNewDate(datePicker) {
-  var person = datePicker.parentElement.parentElement;
-  var array1 = accessArray(person.id);
-  array1["startDate"] = new Date(datePicker.value);
-  // array1["startDate"] = new Date("Aug 15, 2022 15:00:00"); //reset for testing
-  localStorage.setItem(person.id, JSON.stringify(array1));
-  calculate(person.id)
+  let groupsArray = accessGroups();
+  let groupNum = datePicker.parentElement.parentElement.parentElement.id.split('-').pop();
+  let personNum = datePicker.parentElement.id.split('-').pop();
+  groupsArray[groupNum][personNum]["startDate"] = new Date(datePicker.value);
+  localStorage.setItem("groupsArray", JSON.stringify(groupsArray));
+  calculate(groupNum, personNum);
 }
+
+  //var currentDate = new Date("Aug 22, 2022 15:00:00")
+
+  function testA() {
+    // let groupsArray = accessGroups();
+    // groupsArray.splice(1,3);
+    // localStorage.setItem("groupsArray", JSON.stringify(groupsArray));
+    console.log(accessGroups());
+  }
